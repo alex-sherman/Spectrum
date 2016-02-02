8bd86fb924fdf9535978984cdbca08ca4df8ecb6
Alex Sherman <asherman1024@gmail.com>
2016-02-01 23:31:59 -0600

    Renamed GroundInfo to CollisionInfo which should be used more places maybe

--- Framework/Physics/Collision/GroundInfo.cs
+++ Framework/Physics/Collision/GroundInfo.cs
@@ -7,20 +7,21 @@ using System.Text;
 
 namespace Spectrum.Framework.Physics.Collision
 {
-    public class GroundInfo
+    public class CollisionInfo
     {
         public GameObject Object { get; private set; }
-        public Plane Plane { get; private set; }
+        public Vector3 Normal { get; private set; }
         public Vector3 Point { get; private set; }
-        public GroundInfo(GameObject other, Vector3 point, Vector3 normal)
+        public CollisionInfo(GameObject other, Vector3 point, Vector3 normal)
         {
             Point = point;
             Object = other;
-            Plane = new Plane(normal, Vector3.Dot(point, normal));
+            Normal = normal;
         }
         public void Update(Vector3 point, Vector3 normal)
         {
-            Plane = new Plane(normal, Vector3.Dot(point, normal));
+            Point = point;
+            Normal = normal;
         }
     }
 }
