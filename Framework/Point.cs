55e4b4b36bf3008b29115bb4eeeb3e617df933a5
Alex Sherman <asherman1024@gmail.com>
2021-05-23 01:09:14 -0500

    Add Equality/Hash operators to Point

--- Framework/Point.cs
+++ Framework/Point.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Spectrum.Framework
 {
-    public struct Point
+    public struct Point : IEquatable<Point>
     {
         public int X { get; set; }
         public int Y { get; set; }
@@ -15,9 +15,41 @@ namespace Spectrum.Framework
         public Point(int x, int y) { X = x; Y = y; }
         public static explicit operator Vector2(Point point) => new Vector2(point.X, point.Y);
         public static explicit operator Point(Vector2 vector) => new Point() { X = (int)Math.Floor(vector.X), Y = (int)Math.Floor(vector.Y) };
+
         public override string ToString()
         {
             return $"{X},{Y}";
         }
+
+        #region Equality
+        public static bool operator ==(Point left, Point right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Point point && Equals(point);
+        }
+
+        public bool Equals(Point other)
+        {
+            return X == other.X &&
+                   Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
+        #endregion
     }
 }
